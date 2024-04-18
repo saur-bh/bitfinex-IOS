68ae95f0f62214c47b8c8e19289aafc6d0a14816
Saurabh Verma <100rabh@Saurabhs-MacBook-Pro.local>
2024-04-18 08:45:03 +0200

    first

---
+++ test/specs/smoke.js
@@ -0,0 +1,15 @@
+
+describe("Smoke Test",()=>{
+
+    it("Login: User should able to login via API key  ",async()=>{
+
+        const el1=await driver.$('~close_icon');
+        await el1.click();
+        
+
+     
+    });
+    
+
+
+ });
